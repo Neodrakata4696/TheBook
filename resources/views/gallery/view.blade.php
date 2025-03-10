aed6619f5973ac862cfe3cc9f08e491413663605
Neodrakata4696 <neodrakata@hotmail.com>
2025-03-10 11:21:30 +0900

    blade Update

--- resources/views/gallery/view.blade.php
+++ resources/views/gallery/view.blade.php
@@ -1,10 +1,10 @@
 <section>
     <div id="galleryBox">
         <div class="my-4">
-            <div class="flex flex-wrap -m-4">
+            <div class="flex flex-wrap -m-4 items-center">
                 @foreach($images as $image)
-                <button class="uploaded_imagen" type="button" value="{{ $image->path }}">
-                    <img src="{{ asset($image->path) }}" class="m-4 w-full min-w-44 max-w-44 h-full min-h-44 max-h-44">
+                <button class="uploaded_imagen m-4 w-full max-w-44 h-full max-h-44" type="button" value="{{ $image->path }}" >
+                    <img src="{{ asset($image->path) }}" class="w-full h-full">
                 </button>
                 @endforeach
             </div>
