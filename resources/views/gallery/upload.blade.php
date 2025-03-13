0051369bb3f387e3b859c8a7e0aba4ea9a061f31
Neodrakata4696 <neodrakata@hotmail.com>
2025-03-13 14:27:31 +0900

    Image Upload Required

--- resources/views/gallery/upload.blade.php
+++ resources/views/gallery/upload.blade.php
@@ -3,7 +3,7 @@
         <div class="m-4">
             <form method="post" action="{{ route('img.upload') }}" enctype="multipart/form-data">
                 @csrf
-                <input type="file" name="uploaded_image" accept="image/png, image/jpeg">
+                <input type="file" name="uploaded_image" accept="image/png, image/jpeg" required>
                 <button class="bg-gray-200 px-4 float-right">アップロード</button>
             </form>
         </div>
