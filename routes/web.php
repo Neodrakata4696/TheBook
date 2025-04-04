b93afbbe3e839fd509953f20f3c970610a3a64fb
Neodrakata4696 <neodrakata@hotmail.com>
2025-04-04 10:50:10 +0900

    route update

--- routes/web.php
+++ routes/web.php
@@ -56,8 +56,8 @@ Route::group(['middleware' => 'auth'], function() {
     Route::post('/gallery', [ImageController::class, 'upload'])->name('img.upload');
 });
 
-Route::get('/list/printChara', [CharacterController::class, 'printList'])->name('printList');
-Route::get('/list/users/{user}/printChara', [CharacterController::class, 'printListByUser'])->name('printListByUser');
+Route::get('/print', [CharacterController::class, 'printList'])->name('printList');
+Route::get('/users/{user}/print', [CharacterController::class, 'printListByUser'])->name('printListByUser');
 
 //Route::get('/charaList/{chara}', [CharacterController::class, 'detail'])->name('charas.detail');
 
